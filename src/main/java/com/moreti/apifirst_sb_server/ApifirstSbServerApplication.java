c90b2f8d327787c1eae0a4395c203c376abaca3b
kmoreti <kleber.moreti.camargo@gmail.com>
2025-11-08 21:54:41 +0000

    first commit

---
+++ src/main/java/com/moreti/apifirst_sb_server/ApifirstSbServerApplication.java
@@ -0,0 +1,13 @@
+package com.moreti.apifirst_sb_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApifirstSbServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApifirstSbServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moreti/apifirst_sb_server/ApifirstSbServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moreti.apifirst_sb_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifirstSbServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
