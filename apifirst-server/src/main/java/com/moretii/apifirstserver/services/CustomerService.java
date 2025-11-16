9d65049d610cce82f29afdf2bafeed567e3fc3ba
kmoreti <kleber.moreti.camargo@gmail.com>
2025-11-16 17:48:06 +0000

    Add get customers list endpoint

---
+++ apifirst-server/src/main/java/com/moretii/apifirstserver/services/CustomerService.java
@@ -0,0 +1,9 @@
+package com.moretii.apifirstserver.services;
+
+import com.moreti.apifirst.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> listCustomers();
+}
---
+++ apifirst-server/src/main/java/com/moretii/apifirstserver/services/CustomerServiceImpl.java
@@ -0,0 +1,23 @@
+package com.moretii.apifirstserver.services;
+
+import com.moreti.apifirst.model.Customer;
+import com.moretii.apifirstserver.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public List<Customer> listCustomers() {
+        return StreamSupport.stream(customerRepository.findAll().spliterator(), false).toList();
+    }
+}
